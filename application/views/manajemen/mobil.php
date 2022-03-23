b2dd0e3a17619a72c7033890f7973b9954e9ed8b
efrizall <rizal.ef340@gmail.com>
2022-03-24 06:36:41 +0700

    add: view manajemen mobil

--- application/views/manajemen/mobil.php
+++ application/views/manajemen/mobil.php
@@ -1,16 +1,22 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Manajemen Mobil</h1>
-
     <!-- DataTales Example -->
     <div class="row">
-        <div class="col-lg-7">
+        <div class="col">
+            <div class="row">
+                <div class="col-sm-4">
+                </div>
+                <div class="col my-auto">
+                    <a class="float-right btn btn btn-primary mb-3" type="button" href="<?= base_url('admin/mTambah') ?>">
+                    <i class="fas fa-fw fa-plus"></i> Tambah
+                    </a>                        
+                </div>
+            </div>
 
             <div class="card mb-4">
                 <div class="card-header py-3">
-                    <h3 class="m-0 text-primary">Ekspedisi</h3>
+                    <h3 class="m-0 text-primary">Manajemen Mobil</h3>
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
@@ -18,15 +24,27 @@
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>Jenis Mobil</th>
+                                    <th>Tipe</th>
+                                    <th>Nopol</th>
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td>1</td>
-                                    <td>$320,800</td>
-                                    <td><button class="btn btn-primary">Aksi</button></td>
+                                    <td>Toyota Innova</td>
+                                    <td>B 2753 STM</td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary btn-sm dropdown-toggle" type="button" id="aksi" data-toggle="dropdown" aria-expanded="false">
+                                                <i class="fas fa-fw fa-list"></i>
+                                            </button>
+                                            <ul class="dropdown-menu" aria-labelledby="aksi">
+                                                <li><a class="dropdown-item" href="<?= base_url('admin/mEdit') ?>"><i class="fas fa-fw fa-edit mr-2"></i> Edit</a></li>
+                                                <li><a class="dropdown-item text-danger tombol-hapus" href="tes"><i class="fas fa-fw fa-trash mr-2"></i> Hapus</a></li>
+                                            </ul>
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
