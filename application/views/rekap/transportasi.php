1043a4ae4a244d10f976c5c910e1a9ced7bef04d
efrizall <rizal.ef340@gmail.com>
2022-03-24 05:47:37 +0700

    add: view rekap transportasi

--- application/views/rekap/transportasi.php
+++ application/views/rekap/transportasi.php
@@ -1,11 +1,76 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
+    <form action="" method="post" class="none-print">
+        <div class="row">
+            <div class="col-3"><p>Dari tanggal</p></div>
+            <div class="col-3"><p>Sampai</p></div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="form-group">
+                <input type="date" class="form-control" name="mulai">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                <input type="date" class="form-control" name="akhir">
+                </div>
+            </div>
+            <div class="col">
+                <button type="submit" class="btn btn-primary" name="submit">Tampilkan</button>
+            </div>
+        </div>
+    </form>
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Transportasi R</h1>
+    <div class="row">
+        <div class="col">
+            <button class="btn btn-primary mb-3 float-right" type="button" onclick="window.print()">
+                <i class="fas fa-fw fa-print"></i> Cetak Report
+            </button>
+    </div>
+
+    <div class="card mb-4">
+        <div class="card-header py-3">
+            <h3 class="judul h3 text-gray-800">Rekapitulasi Transportasi</h3>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped display" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Tanggal Permintaan</th>
+                            <th>Nomor Permintaan</th>
+                            <th>Nama</th>
+                            <th>Divisi</th>
+                            <th>Jenis Mobil</th>
+                            <th>Tujuan</th>
+                            <th>Keperluan</th>
+                            <th>Tanggal Pakai</th>
+                            <th>Tanggal Kembali</th>
+                            <th>Jam Pakai</th>
+                            <th class="td">Jam Kembali</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td class="td"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 </div>
 <!-- /.container-fluid -->
-
-</div>
-<!-- End of Main Content -->
