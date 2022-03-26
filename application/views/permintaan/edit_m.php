972b95f642146ab4fae9e0ba40e73714dc672450
efrizall <rizal.ef340@gmail.com>
2022-03-26 13:56:57 +0700

    update: view edit maintenance

--- application/views/permintaan/edit_m.php
+++ application/views/permintaan/edit_m.php
@@ -8,32 +8,33 @@
         <div class="card-body">
             <form action="" method="post">
                 <div class="row">
+                    <input type="text" name="id" value="<?= $data['id'] ?>" hidden>
                     <div class="col">
                         <label class="" for="nama">Nama</label>
-                        <input type="text" class="form-control" name="nama" id="nama" placeholder="Efrizal">
+                        <input type="text" class="form-control" name="nama" id="nama" value="<?= $data['nama'] ?>">
                         <?= form_error('nama', '<small class="text-danger">', '</small>'); ?>
                     </div>
                     <div class="col">
                         <label class="" for="divisi">Divisi</label>
-                        <input type="text" class="form-control" name="divisi" id="divisi" placeholder="Pelayanan Strategis SDM & Umum">
+                        <input type="text" class="form-control" name="divisi" id="divisi" value="<?= $data['divisi'] ?>">
                         <?= form_error('divisi', '<small class="text-danger">', '</small>') ?>
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col">
                         <label class="" for="permintaan">Permintaan</label>
-                        <input type="text" class="form-control" name="permintaan" id="permintaan" placeholder="Perbaikan lampu">
+                        <input type="text" class="form-control" name="permintaan" id="permintaan" value="<?= $data['permintaan'] ?>">
                         <?= form_error('permintaan', '<small class="text-danger">', '</small>') ?>
                     </div>
                     <div class="col">
                         <label class="" for="ket">Keterangan atau Barang</label>
-                        <input type="text" class="form-control" name="keterangan" id="ket" placeholder="Lampu Philips 15 W">
+                        <input type="text" class="form-control" name="keterangan" id="ket" value="<?= $data['keterangan'] ?>">
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col">
                         <label for="tgl_surat">Tanggal Permintaan</label>
-                        <input type="date" class="form-control" id="tanggal" name="tanggal" placeholder="2022-03-23">
+                        <input type="date" class="form-control" id="tanggal" name="tanggal" value="<?= $data['tanggal'] ?>">
                         <!-- <label class="" for="tanggal">Tanggal Permintaan</label>
                         <input type="text" class="form-control" name="tanggal" id="tanggal"> -->
                         <?= form_error('tanggal', '<small class="text-danger">', '</small>') ?>
@@ -41,7 +42,7 @@
                     <div class="col">
                         <label class="" for="pemeriksa">Pemeriksa</label>
                         <select name="pemeriksa" id="pemeriksa" class="custom-select">
-                            <option selected disabled value="">AVP Pelayanan Strategis SDM & Umum</option>
+                            <option selected disabled>Pilih Pemeriksa..</option>
                             <option value="AVP Pendukung Direksi & Kesekretariatan">
                                 <b>AVP Pendukung Direksi & Kesekretariatan</b>
                             </option>
@@ -51,49 +52,49 @@
                             <option value="AVP TJSL & CSR">
                                 <b>AVP TJSL & CSR</b>
                             </option>
-                            <option value="AVP Hukum Bisnis & Kepatuhan" >
+                            <option value="AVP Hukum Bisnis & Kepatuhan">
                                 <b>AVP Hukum Bisnis & Kepatuhan</b>
                             </option>
-                            <option value="AVP Penasihat Hukum & Litigasi" >
+                            <option value="AVP Penasihat Hukum & Litigasi">
                                 <b>AVP Penasihat Hukum & Litigasi</b>
                             </option>
-                            <option value="AVP Strategi Korporasi & Transformasi Organisasi" >
+                            <option value="AVP Strategi Korporasi & Transformasi Organisasi">
                                 <b>AVP Strategi Korporasi & Transformasi Organisasi</b>
                             </option>
-                            <option value="AVP Pengendalian Kinerja Korporasi" >
+                            <option value="AVP Pengendalian Kinerja Korporasi">
                                 <b>AVP Pengendalian Kinerja Korporasi</b>
                             </option>
-                            <option value="AVP Pengembangan Usaha & Portofolio" >
+                            <option value="AVP Pengembangan Usaha & Portofolio">
                                 <b>AVP Pengembangan Usaha & Portofolio</b>
                             </option>
-                            <option value="AVP Riset & Inovasi" >
+                            <option value="AVP Riset & Inovasi">
                                 <b>AVP Riset & Inovasi</b>
                             </option>
-                            <option value="AVP Keuangan" >
+                            <option value="AVP Keuangan">
                                 <b>AVP Keuangan</b>
                             </option>
-                            <option value="AVP Tresuri" >
+                            <option value="AVP Tresuri">
                                 <b>AVP Tresuri</b>
                             </option>
-                            <option value="AVP Akuntansi" >
+                            <option value="AVP Akuntansi">
                                 <b>AVP Akuntansi</b>
                             </option>
-                            <option value="AVP Anggaran" >
+                            <option value="AVP Anggaran">
                                 <b>AVP Anggaran</b>
                             </option>
-                            <option value="AVP Operasional & Pengelolaan Informasi Aset" >
+                            <option value="AVP Operasional & Pengelolaan Informasi Aset">
                                 <b>AVP Operasional & Pengelolaan Informasi Aset</b>
                             </option>
-                            <option value="AVP Strategi & Sinergi Aset" >
+                            <option value="AVP Strategi & Sinergi Aset">
                                 <b>AVP Strategi & Sinergi Aset</b>
                             </option>
-                            <option value="AVP Optimalisasi & Komersialisasi Aset" >
+                            <option value="AVP Optimalisasi & Komersialisasi Aset">
                                 <b>AVP Optimalisasi & Komersialisasi Aset</b>
                             </option>
-                            <option value="AVP Pelayanan Strategis SDM & Umum" >
+                            <option value="AVP Pelayanan Strategis SDM & Umum">
                                 <b>AVP Pelayanan Strategis SDM & Umum</b>
                             </option>
-                            <option value="AVP Pendukung Strategik" >
+                            <option value="AVP Pendukung Strategik">
                                 <b>AVP Pendukung Strategik</b>
                             </option>
                         </select>
