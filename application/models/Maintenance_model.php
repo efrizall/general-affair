929ddfd0bd696eec5cdad3adbd175d35d24b5b82
efrizall <rizal.ef340@gmail.com>
2022-03-26 22:25:13 +0700

    update: nama funct

--- application/models/Maintenance_model.php
+++ application/models/Maintenance_model.php
@@ -40,47 +40,6 @@ class Maintenance_model extends CI_Model
         $this->db->insert('maintenance', $data);
     }
 
-    public function tambahTransportasi()
-    {
-        $data = [
-            'nama' => $this->input->post('nama', true),
-            'divisi' => $this->input->post('divisi', true),
-            'mobil_id' => $this->input->post('permintaan', true),
-            'tujuan' => $this->input->post('keterangan', true),
-            'keperluan' => 'Belum diproses',
-            'tgl_pakai' => $this->input->post('tanggal', true),
-            'tgl_kembali' => 1,
-            'jam_pakai' => $this->input->post('tanggal', true),
-            'jam_kembali' => $this->input->post('tanggal', true),
-            'pemeriksa' => $this->input->post('tanggal', true),
-            'ttd_avp' => 'Belum Disetujui',
-            'ttd_pemeriksa' => 'Belum Disetujui',
-            'status' => 'Belum Dikembalikan',
-            'tanggal' => $this->input->post('tanggal', true),
-            'file' => 'Tidak ada',
-            'user_id' => 1
-        ];
-
-        $this->db->insert('transportasi', $data);
-    }
-
-    public function tambahEkspedisi()
-    {
-        $data = [
-            'nama' => $this->input->post('nama', true),
-            'divisi' => $this->input->post('divisi', true),
-            'permintaan' => $this->input->post('permintaan', true),
-            'keterangan' => $this->input->post('keterangan', true),
-            'status' => 'Belum diproses',
-            'tanggal' => $this->input->post('tanggal', true),
-            'app_maintenance_id' => 1,
-            'file' => 'Tidak ada',
-            'user_id' => 1
-        ];
-
-        $this->db->insert('maintenance', $data);
-    }
-
     public function editMaintenance($id)
     {
         $data = [
@@ -95,32 +54,10 @@ class Maintenance_model extends CI_Model
         $this->db->update('maintenance', $data);
     }
 
-    public function editTransportasi($id)
+    public function hapusMaintenance($id)
     {
-        $data = [
-            'nama' => $this->input->post('nama', true),
-            'divisi' => $this->input->post('divisi', true),
-            'permintaan' => $this->input->post('permintaan', true),
-            'keterangan' => $this->input->post('keterangan', true),
-            'tanggal' => $this->input->post('tanggal', true),
-            'pemeriksa' => $this->input->post('pemeriksa', true),
-        ];
         $this->db->where('id', $id);
-        $this->db->update('transportasi', $data);
-    }
-
-    public function editEkspedisi($id)
-    {
-        $data = [
-            'nama' => $this->input->post('nama', true),
-            'divisi' => $this->input->post('divisi', true),
-            'permintaan' => $this->input->post('permintaan', true),
-            'keterangan' => $this->input->post('keterangan', true),
-            'tanggal' => $this->input->post('tanggal', true),
-            'pemeriksa' => $this->input->post('pemeriksa', true),
-        ];
-        $this->db->where('id', $id);
-        $this->db->update('maintenance', $data);
+        $this->db->delete('maintenance');
     }
 
     public function tambahProses($id, $nama, $divisi)
@@ -152,52 +89,6 @@ class Maintenance_model extends CI_Model
         $data = [
             'status' => $this->input->post('status', true)
         ];
-        $this->db->where('id', $id);
-        $this->db->update('maintenance', $data);
-    }
-
-    public function hapusMaintenance($id)
-    {
-        $this->db->where('id', $id);
-        $this->db->delete('maintenance');
-    }
-
-    public function ttdAvp($id)
-    {
-        $data = [
-            'ttd_avp' => 'Disetujui'
-        ];
-
-        $this->db->where('id', $id);
-        $this->db->update('maintenance', $data);
-    }
-
-    public function ttdPemeriksa($id)
-    {
-        $data = [
-            'ttd_pemeriksa' => 'Disetujui'
-        ];
-
-        $this->db->where('id', $id);
-        $this->db->update('maintenance', $data);
-    }
-
-    public function tolakAvp($id)
-    {
-        $data = [
-            'ttd_avp' => 'Tidak Disetujui'
-        ];
-
-        $this->db->where('id', $id);
-        $this->db->update('maintenance', $data);
-    }
-
-    public function tolakPemeriksa($id)
-    {
-        $data = [
-            'ttd_pemeriksa' => 'Tidak Disetujui'
-        ];
-
         $this->db->where('id', $id);
         $this->db->update('maintenance', $data);
     }
