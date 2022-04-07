e56db9d66aa41f7acd1f70a41139b5d334713753
efrizall <rizal.ef340@gmail.com>
2022-04-07 14:49:51 +0700

    update: dashboard user umum

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,11 +2,12 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion none-print" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center my-3" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                    <!-- <i class="fas fa-laugh-wink"></i> -->
+                    <i class="fas fa-book"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">General <br> Affair</div>
             </a>
 
             <!-- Divider -->
--- application/views/umum/index.php
+++ application/views/umum/index.php
@@ -2,10 +2,28 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h4 mb-4 text-gray-800">Flowchart Pengajuan, Approval, dan Tindaklanjut</h1>
+    <!-- <h1 class="h4 mb-4 text-gray-800">Flowchart Pengajuan, Approval, dan Tindaklanjut</h1> -->
 
-    <img src="<?= base_url('assets/img/alur.jpg') ?>" alt="" srcset="" class="mx-auto d-block" style="width: 90%;">
     
+    <div class="row">
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-s font-weight-bold text-primary text-uppercase mb-3">
+                                Flowchart Pengajuan, Approval, dan Tindaklanjut
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <img src="<?= base_url('assets/img/alur.jpg') ?>" alt="" srcset="" class="mx-auto d-block" style="width: 100%;">    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     
 
