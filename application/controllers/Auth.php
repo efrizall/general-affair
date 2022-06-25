516545c6d9b8bead4fcf7a54b25ab115e82aedc1
efrizall <rizal.ef340@gmail.com>
2022-06-25 23:53:35 +0700

    add: funct time server

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,13 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
+class Auth extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
     }
 
-    public function index(){
+    public function index()
+    {
         // Rules
         // Mengecek semua input sudah sesuai rules
         $this->form_validation->set_rules('nip', 'NIP', 'trim|required');
@@ -23,13 +25,14 @@ class Auth extends CI_Controller {
         }
     }
 
-    private function _login(){
+    private function _login()
+    {
         $nip = $this->input->post('nip');
         $password = $this->input->post('password');
 
         $user = $this->db->get_where('user', ['nip' => $nip])->row_array();
-        if($user){
-            if(password_verify($password, $user['password'])){
+        if ($user) {
+            if ($password == $user['password']) {
                 // Mengirim data menggunakan session
                 $data = [
                     'id' => $user['id'],
@@ -38,25 +41,25 @@ class Auth extends CI_Controller {
                     'nip' => $user['nip'],
                     'role_id' => $user['role_id']
                 ];
-                
+
                 $this->session->set_userdata($data);
-                $this->session->set_userdata('logged',true);
+                $this->session->set_userdata('logged', true);
 
                 // cek role
-                if($user['role_id'] == 1){
+                if ($user['role_id'] == 1) {
                     redirect('admin');
-                }elseif($user['role_id'] == 2){
+                } elseif ($user['role_id'] == 2) {
                     redirect('pemeriksa');
-                }elseif($user['role_id'] == 3){
+                } elseif ($user['role_id'] == 3) {
                     redirect('staff');
-                }else{
+                } else {
                     redirect('umum');
                 }
-            }else{
+            } else {
                 $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Password salah</div>');
                 redirect(base_url(''));
             }
-        }else{
+        } else {
             $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">NIP tidak terdaftar</div>');
             redirect(base_url(''));
         }
